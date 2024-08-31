8ecb6f44abf08a3e76a22e3da6412ae0ad2d5f4f
sipunpanda <sipunpanda26@gmail.com>
2024-08-31 18:41:31 +0530

    middleware setup

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // require('dotenv').config({path:'./env'})
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
-
+import app from "./app.js";
 
 dotenv.config({
     path:'./env'
@@ -11,3 +11,12 @@ dotenv.config({
 
 //connect to db
 connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, ( => {
+        console.log(`Server running on port ${process.env.PORT}`);  //process.env.PORT is environment variable set in .env file. It will return port number if set or 8000 if not set.
+    }))
+})
+.catch((err) => {
+    console.log("mongo db connection failed: ", err);
+    
+})
