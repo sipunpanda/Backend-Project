e0ae8559058ca758b22bbb3a900a4e8c20ef1614
sipunpanda <sipunpanda26@gmail.com>
2024-09-01 04:23:47 +0530

    token correction

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -67,7 +67,7 @@ userSchema.methods.isPasswordCorrect = async function (password) {
 }
 
 userSchema.methods.generateAccessToken =  function() {
-    jwt.sign(
+  return  jwt.sign(
         {
             _id: this._id,
             email: this.email,
@@ -83,7 +83,7 @@ userSchema.methods.generateAccessToken =  function() {
 }
 
 userSchema.methods.generateRefreshToken = function(){ 
-    jwt.sign(
+   return jwt.sign(
         {
             _id: this._id,
             
