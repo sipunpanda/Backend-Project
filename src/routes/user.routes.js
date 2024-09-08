27d63864ac220f4dcf7decc938926d9cf0dd8dbb
sipunpanda <sipunpanda26@gmail.com>
2024-09-08 22:10:33 +0530

    "Added new routes and controllers to user.routes.js, including change password, get current user, update account details, update avatar and cover image, get user channel profile, and get watch history."

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,18 @@
 import Router from "express";
-import {loginUser, logoutUser, refreshAccessToken, registerUser} from "../controllers/user.controller.js"
-import {upload} from  "../middlewares/multer.middleware.js"
+import {
+    changeCurrentPassword,
+    getCurrentUser,
+    getUserChannelProfile,
+    getWatchHistory,
+    loginUser,
+    logoutUser,
+    refreshAccessToken,
+    registerUser,
+    updateAccountDetails,
+    updateUserAvatar,
+    updateUserCoverImage
+} from "../controllers/user.controller.js"
+import { upload } from "../middlewares/multer.middleware.js"
 import { varifyJWT } from "../middlewares/auth.middleware.js";
 
 
@@ -24,10 +36,23 @@ router.route("/register").post(
 router.route("/login").post(loginUser)
 
 //secured routes
-router.route("/logout").post(varifyJWT, logoutUser) 
+router.route("/logout").post(varifyJWT, logoutUser)
+
 router.route("/refresh-token").post(refreshAccessToken)
 
+router.route("/change-password").post(varifyJWT, changeCurrentPassword)
 
+router.route("/current-user").get(varifyJWT, getCurrentUser)
+
+router.route("/update-account-details").patch(varifyJWT, updateAccountDetails)
+
+router.route("/update-avatar").patch(varifyJWT, upload.single("avatar"), updateUserAvatar)
+
+router.route("/update-coverImage").patch(varifyJWT, upload.single("coverImage"), updateUserCoverImage)
+
+router.route("/c/:username").get(varifyJWT, getUserChannelProfile)// hence it is an param so we shoul write"/name/:parameter_name"
+
+router.route("/watchHistory").get(varifyJWT, getWatchHistory)
 
 export default router
 
